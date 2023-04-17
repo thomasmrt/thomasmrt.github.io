dc7e6241a068a095549dfbe7014c79e649e13a16
bcmb78 <bcmb.webmaster@gmail.com>
2023-04-17 20:50:10 +0200

    Update config.js

--- tina/config.js
+++ tina/config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from "tinacms";
 
-// Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
-  branch,
+  branch: main,
   clientId: "b2e8e05a-3ce9-4af9-add5-027cdbde4932", // Get this from tina.io
   token: "8f2190f106db92dfbbd154da0d89d7ae81052886", // Get this from tina.io
 
